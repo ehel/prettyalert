42a8a76be1ff1e4f7e7e17b9ae102bc3d1cadc66
ValentinSirbu <valentinsirbu@yandex.com>
2015-09-14 01:33:23 +0300

    Add button customization

--- prettyalert.js
+++ prettyalert.js
@@ -7,7 +7,7 @@ function createfg(htmlStr) {
     }
     return frag;
 }
-function insertHtml(title, content, type) {
+function insertHtml(title, content, type,btn) {
     var attention = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><path id="svgico" fill="none" stroke="#FBB03B" stroke-width="8" stroke-linecap="round" stroke-miterlimit="10" d="M50,83v2 M50,14v54"/></svg>';
     var error = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><path id="svgico" fill="#FFFFFF" stroke="#C1272D" stroke-width="8" stroke-linecap="round" stroke-miterlimit="10" d="M8.8,90.7L90,9 M90,90.7 L8.8,9"/></svg>';
     var succes = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"><path id="svgico" fill="#FFFFFF" stroke="#8CC63F" stroke-miterlimit="10" d="M46.6,64.3l33.7-40L47.4,75.4L24.7,56L46.6,64.3z" /></svg>';
@@ -19,7 +19,7 @@ function insertHtml(title, content, type) {
     } else {
         svg = attention;
     }
-
+    var button = '<a id="closeModal" href="#" >'+btn+'</a>';
     var htmlStr = '<div id="overlay"><div id="modalcontainer">' + svg + '<h2>' + title + '</h2><p>' + content + '</p><a id="closeModal" href="#" >OK</a></div></div>';
     var fragment = createfg(htmlStr);
     
